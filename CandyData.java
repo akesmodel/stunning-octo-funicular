5ed9faecd8ef1ca126dad57fbb92e7ee351e05d7
Abby Kesmodel <akesmodel@greenwichacademy.org>
2025-10-24 09:48:45 -0400

    hi miss bono

--- CandyData.java
+++ CandyData.java
@@ -7,7 +7,6 @@ import java.util.*;
 public class CandyData{
     public static void main(String[] args) throws FileNotFoundException{
         highSugarVslowSugar("/Users/akesmodel/Desktop/Cs_Seminar/stunning-octo-funicular/candy-data.csv");
-        chocVsnonchoc("/Users/akesmodel/Desktop/Cs_Seminar/stunning-octo-funicular/candy-data.csv");
     }
     public static void highSugarVslowSugar(String pathname) throws FileNotFoundException{
         File f= new File(pathname);
@@ -51,45 +50,6 @@ public class CandyData{
         else{
             System.out.println("Tie!");
         }
-
-
-    }
-    
-    
-    public static void chocVsnonchoc(String pathname) throws FileNotFoundException{
-        File f= new File(pathname);
-        Scanner sc= new Scanner(f);
-
-        String[] header=sc.nextLine().split(" ");
-        int choc_idx = 1;
-        int win_idx=12;
-
-        ArrayList<Double> chocWins=new ArrayList();
-        ArrayList<Double> nonChocWins=new ArrayList();
-
-
-        while (sc.hasNextLine()){
-            ArrayList<String> line= new ArrayList<>(Arrays.asList(sc.nextLine().split(",")));
-            if(Double.parseDouble(line.get(choc_idx))==1){
-                chocWins.add(Double.parseDouble(line.get(win_idx)));
-            }
-            else{
-                nonChocWins.add(Double.parseDouble(line.get(win_idx)));
-            }
-        }
-
-        double choc_avg=0;
-        for(Double d: chocWins){
-            choc_avg+=d;
-        }
-        double nonChoc_avg=0;
-        for(Double d: nonChocWins){
-            nonChoc_avg+=d;
-        }
-        choc_avg/=chocWins.size();
-        nonChoc_avg/=nonChocWins.size();
-        System.out.println(choc_avg);
-        System.out.println(nonChoc_avg);
 
 
     }
